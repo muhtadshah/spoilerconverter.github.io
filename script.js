c3ddf21d0fb2a6055879410ebb81554118689534
Muhtad Shah <80309124+muhtadshah@users.noreply.github.com>
2023-06-08 14:09:00 +0530

    Refactor JavaScript code
    This commit refactors the existing JavaScript code for the website and adds important comments to improve code readability and maintainability.

--- script.js
+++ script.js
@@ -1,82 +1,47 @@
+// Function to convert input text to spoilers
 function convertToSpoilers() {
-    const inputText = document.getElementById("inputText").value;
-    let outputText = "";
+  const inputText = document.getElementById("inputText").value;
+  let outputText = "";
 
-    for (let i = 0; i < inputText.length; i++) {
-        outputText += "||" + inputText[i] + "||";
-    }
+  for (let i = 0; i < inputText.length; i++) {
+    outputText += "||" + inputText[i] + "||";
+  }
 
-    document.getElementById("outputText").textContent = outputText;
+  document.getElementById("outputText").textContent = outputText;
 }
 
-function convertToSpoilers() {
-    const inputText = document.getElementById("inputText").value;
-    let outputText = "";
-
-    for (let i = 0; i < inputText.length; i++) {
-        outputText += "||" + inputText[i] + "||";
-    }
-
-    document.getElementById("outputText").textContent = outputText;
-}
-
+// Function to copy the output text to the clipboard
 function copyOutputText() {
-    const outputText = document.getElementById("outputText");
-  
-    // Create a range object to select the text within the outputText element
-    const range = document.createRange();
-    range.selectNode(outputText);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-  
-    // Copy the selected text
-    document.execCommand("copy");
-  
-    // Deselect the text
-    window.getSelection().removeAllRanges();
-  
-    // Change the copy button text briefly
-    const copyButton = document.querySelector(".copy-icon");
-    copyButton.textContent = "Copied!";
-    setTimeout(() => {
-        copyButton.textContent = "Copy";
-    }, 2000);
+  const outputText = document.getElementById("outputText").textContent;
+
+  // Use the Clipboard API to write the text to the clipboard
+  navigator.clipboard.writeText(outputText).then(() => {
+    alert("Copied to clipboard!");
+  });
 }
 
-function convertToSpoilers() {
-    const inputText = document.getElementById("inputText").value;
-    let outputText = "";
-
-    for (let i = 0; i < inputText.length; i++) {
-        outputText += "||" + inputText[i] + "||";
-    }
-
-    document.getElementById("outputText").textContent = outputText;
-}
-
-function copyOutputText() {
-    const outputText = document.getElementById("outputText").textContent;
-    navigator.clipboard.writeText(outputText).then(() => {
-        alert("Copied to clipboard!");
-    });
-}
-
+// Function to close the popup
 function closePopup() {
-    document.getElementById("popup").classList.remove("show");
+  document.getElementById("popup").classList.remove("show");
 }
 
+// Function to show the popup
 function showPopup() {
-    document.getElementById("popup").classList.add("show");
+  document.getElementById("popup").classList.add("show");
 }
 
+// Function to check for spoilers
 function checkForSpoilers() {
-    const inputText = document.getElementById("inputText").value;
-    if (inputText.includes("||")) {
-        showPopup();
-        document.getElementById("outputText").textContent = ""; // Clear the output text
-    } else {
-        convertToSpoilers();
-    }
+  const inputText = document.getElementById("inputText").value;
+
+  // Check if input text contains spoilers
+  if (inputText.includes("||")) {
+    showPopup();
+    document.getElementById("outputText").textContent = ""; // Clear the output text
+  } else {
+    convertToSpoilers();
+  }
 }
 
+// Event listener for input changes
 document.getElementById("inputText").addEventListener("input", checkForSpoilers);
